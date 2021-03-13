9b9fd5352fac7f8c4604a0ce56310b16664f06f6
soonniee <ckstns1231@gmail.com>
2021-03-14 02:01:49 +0900

    Styling

--- src/Movie.js
+++ src/Movie.js
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 function Movie({id,year,title,summary,poster,genres}){
     return( 
-    <div className="movies__movie">
+    <div className="movie">
         <img src={poster} alt={title} title={title}></img>
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <p className="movie__summary">{summary}</p>
-            <ul className = "genres">
+            <p className="movie__summary">{summary.slice(0,160)}...</p>
+            <ul className = "movie__genres">
                 {genres && genres.map((genre,index) =>(
                 <li key={index} className="genres__genre">{genre}
                 </li> 
